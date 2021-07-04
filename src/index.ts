5ec931c8ef8dc2955b3d5602df9229946c5b48e6
Ronald <ronystivens@hotmail.com>
2021-07-04 23:19:24 +0200

    implemented sorting for strings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-//trying if it's working:
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+//testing how it works with number[]
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+//testing how it works with strings
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
