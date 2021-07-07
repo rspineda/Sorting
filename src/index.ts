23a90cbfcf4564740afc006b47e08f8b3c00dcee
Ronald <ronystivens@hotmail.com>
2021-07-07 23:00:54 +0200

    implemented linkedList sorting with interface, and working fine

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from "./LinkedList";
 
 //testing how it works with number[]
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
@@ -9,7 +10,18 @@ import { CharactersCollection } from './CharactersCollection';
 // console.log(numbersCollection.data);
 
 //testing how it works with strings
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+//testing how it works with LinkedList:
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
